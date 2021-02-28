7bfac524431e572ede4e0f73bdd2e36223a34d8d
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2021-02-28 23:06:29 +0900

    add presets
    textlint-rule-preset-jtf-style
    textlint-rule-preset-ja-engineering-paper

--- core-ja/index.js
+++ core-ja/index.js
@@ -1,12 +1,18 @@
 const { moduleInterop } = require("@textlint/module-interop");
 
 const presetJaTechnical = moduleInterop(require("textlint-rule-preset-ja-technical-writing"));
+const presetJTF = moduleInterop(require("textlint-rule-preset-jtf-style"));
+const presetJaEngineeringPaper = moduleInterop(require("textlint-rule-preset-ja-engineering-paper"));
 
 module.exports = {
   rules: {
+      ...presetJTF.rules,
       ...presetJaTechnical.rules,
+      ...presetJaEngineeringPaper.rules,
   },
   rulesConfig: {
+      ...presetJTF.rulesConfig,
       ...presetJaTechnical.rulesConfig,
+      ...presetJaEngineeringPaper.rulesConfig,
   }
 };

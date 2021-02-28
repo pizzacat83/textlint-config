65e7a2f6c3b98728578b4c0404eb6013571d60b6
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2021-03-01 00:38:48 +0900

    add spacing rule

--- configs/lib/ja/index.js
+++ configs/lib/ja/index.js
@@ -11,5 +11,14 @@ module.exports = {
     "textlint-rule-preset-jtf-style": true,
     "textlint-rule-preset-ja-technical-writing": true,
     "preset-ja-engineering-paper" : true,
+    "preset-ja-spacing": {
+      "ja-space-between-half-and-full-width": {
+        "space": "always",
+      },
+      "ja-space-around-code": true,
+      "ja-no-space-around-parentheses": true,
+      "ja-space-after-exclamation": false,
+      "ja-space-after-question": false,
+    }
   },
 }

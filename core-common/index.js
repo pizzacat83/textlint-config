69adc626563bdaed31e49d4d970e1d4222143089
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2021-02-28 23:11:52 +0900

    add preset textlint-filter-rule-comments

--- core-common/index.js
+++ core-common/index.js
@@ -1,8 +1,12 @@
 const { moduleInterop } = require("@textlint/module-interop");
 
+const presetFilterComments = moduleInterop(require("textlint-filter-rule-comments"));
+
 module.exports = {
   rules: {
+    ...presetFilterComments.rules,
   },
   rulesConfig: {
+    ...presetFilterComments.rulesConfig,
   }
 };
